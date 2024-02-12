727902526df451e1c72812e690f3c7d13bb2f774
Abigail Beck <abbie.beck@gmail.com>
2024-02-12 17:22:37 +0000

    Small Problem 1-3 & Documentation Solutions

--- small_problems/easy_3/searching_101.rb
+++ small_problems/easy_3/searching_101.rb
@@ -1,8 +1,3 @@
-=begin
-Write a program that solicits 6 numbers from the user, then prints a message 
-that describes whether or not the 6th number appears amongst the first 5 numbers.
-=end 
-
 def searching
   first_five = []
   puts "Enter the 1st number:"
